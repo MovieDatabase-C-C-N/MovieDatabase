543c8c5dca6ac15e72cdf57c892484d19137fe93
Cody Hodges <hodges.cody00@gmail.com>
2022-02-10 16:15:43 -0600

    Push

--- js/js.js
+++ js/js.js
@@ -1,21 +1,26 @@
 const glitchURL = "https://tartan-quill-libra.glitch.me/movies";
 
-renderMovies();
+let movieDB = $.get(glitchURL);
+    movieDB.done(function(data){
+    console.log(data)
+        renderMovies(data)
+})
+
 
 // GENERATE MOVIE CARDS
-function renderMovies() {
-    $.get(glitchURL).done(function (data) {
-        console.log(data)
-        for (let i = 0; i < data.length; i++) {
+function renderMovies(db) {
+
+        for (let i = 0; i < db.length ; i++) {
             let filmCard = `
                                     <div class="flip-card">
                                         <div class="flip-card-inner">
                                             <div class="flip-card-front">
-                                                <img class="card-img-top" src="http://image.tmdb.org/t/p/original/${data[i].poster_path}" alt="">
+                                                <img class="card-img-top" src="http://image.tmdb.org/t/p/original/${db[i].poster_path}" alt="">
                                             </div>
                                             <div class="flip-card-back">
-                                                <h5 class="movieTitle">${data[i].original_title}</h5>
-                                                <p class="movieText">${data[i].overview} </p>
+                                                <h5 class="movieTitle">${db[i].original_title}</h5>
+                                                <p class="date" >Release date : ${db[i].release_date}</p>
+                                                <p class="movieText">${db[i].overview} </p>
                                                 <button id="edit" type="button">EDIT</button>
                                                 <button id="delete" type="button">DELETE</button>
                                                 <button id="watch" type="button">ADD TO WATCH</button>
@@ -26,36 +31,29 @@ function renderMovies() {
                                                      <span class="fa fa-star"></span>
                                                      <span class="fa fa-star"></span>
                                                 </div>
-                                               
                                             </div>
                                         </div>
                                    </div>`
             $('.movieCards').append(filmCard);
         }
-    });
 }
 
 
+// DELETE BUTTON
+// $('#delete').on('click', )
 
 
-$('#wantToWatch').click(function () {
-    let movieInput = $('#search').val()
+
+
+
+
+
+$('#addMovie').on ('click', function () {
+    let movieInput = $('#addName').val()
     console.log(movieInput)
-    $.get(`https://api.themoviedb.org/3/search/movie?api_key=36bae576330e105013948f7fc0b734c0&language=en-US&query=${movieInput}&page=1&include_adult=false`).done(function (data) {
-        console.log(data);
 
-        const newFilmCard = {
-            title: data.results[0].title,
-            poster: `http://image.tmdb.org/t/p/original${data.results[0].poster_path}`,
-            plot: data.results[0].overview,
-        }
-        const options = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(newFilmCard),
-        }
-        fetch(glitchURL, options)
+    $.get(`https://api.themoviedb.org/3/search/movie?api_key=36bae576330e105013948f7fc0b734c0&language=en-US&query=${movieInput}&page=1&include_adult=false`).done(function (db) {
+        console.log(db);
+        
     })
 })
